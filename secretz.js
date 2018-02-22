ac9079994a38997e9c54ad020f10dc3c3e6b4f01
avorona <vitalii.parshikov@gmail.com>
2018-02-22 11:18:21 +0200

    Add secretz.js solution

--- secretz.js
+++ secretz.js
@@ -1,8 +1,9 @@
 const zlib = require('zlib');
+const ungzip = zlib.createGunzip();
 const tar  = require('tar');
 const parser = new tar.Parse();
 const crypto = require('crypto');
-
+const concat = require('concat-stream')
 
 const cipherName = process.argv[2];
 const passphrase = process.argv[3];
@@ -11,13 +12,19 @@ const decripter = crypto.createDecipher(cipherName, passphrase)
 
 parser.on('entry',  (entry) => {
 
-const hash = crypto.createHash('md5', {encoding: 'hex'});
-const fileName=entry.file
+if (entry.type !== 'File' ) return entry.resume();
 
+const hashHex = crypto.createHash('md5', {encoding: 'hex'});
+const fileName=entry.path;
+
+entry.pipe(hashHex).pipe(concat(function (hash) {
+  console.log(`${hash} ${fileName}`);
+}))
 
 })
 
 process.stdin
 .pipe(decripter)
+.pipe(ungzip)
 .pipe(parser)
-.pipe(process.stdout)
+
